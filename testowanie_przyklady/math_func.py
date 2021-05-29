e6936905d657486a81e00d1a00ae0687a25a9a11
cooklee <sbbogus@gmail.com>
2021-05-29 16:54:34 +0200

    test

---
+++ testowanie_przyklady/math_func.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    assert add(1, 2) == 3
+    assert add(-1, -1) == -2
+    assert add(0, 0) == 0
+    assert add(0, 10) == 0
+    assert add(1, 2) == 3
+    assert add(-1, -1) == -2
+    assert add(0, 0) == 0
+    assert add(0, 10) == 0
+
---
+++ testowanie_przyklady/test.py
@@ -0,0 +1,17 @@
+from testowanie_przyklady.math_func import add
+
+
+def test_add_1_1():
+    assert add(1, 1) == 2
+
+
+def test_add_1_2():
+    assert add(1, 2) == 3
+
+
+def test_add_1_3():
+    assert add(1, 3) == 5
+
+
+def test_add_1_4():
+    assert add(1, 4) == 5
